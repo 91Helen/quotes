119b86919b425a58567c13e7b6b524f3208bd78c
91Helen <elena.filatova.nartova@mail.ru>
2025-02-15 09:08:52 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+
+state = {
+content:null,
+author:null
+}
+
+ async componentDidMount(){
+const url ="https://quotes-api-self.vercel.app/quote";
+const response = await fetch(url);
+const data = await response.json();
+this.setState({content:data.content, author: data.author})
+console.log (data.content, data.author);
+  }
+
+  render() {
+   
+    return (
+      <div>
+      <p>{this.state.content}</p>  
+    <p>{this.state.author}</p>
+      </div>
+    );
+  }
+
+}
+
+export default App;
